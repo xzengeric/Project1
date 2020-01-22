d7b7b8b0a5aeea317ba3605d85fd30e9b85bf7ae
yan zhao <yzhao322@gmail.com>
2020-01-21 19:48:10 -0800

    add some css properties

--- project1.js
+++ project1.js
@@ -3,20 +3,38 @@ $(document).ready(function () {
     var app_id = "98b05697";
     var app_key = "66b497d1c74de44a3ee14c66199e0618";
 
-    $('#submitButton').click(function () {
+    // Added a specific id for the button to attach the handler
+    $("#submitButton").click(function () {
+        // alert("it is running the click handler inside ready function");
         $('h1').hide();
-        $('div').hide();
+        $('.input').hide();
+        // Removing the "hide", so the search area can still be reused with reloading the page
+        //$('h1').hide();
+        //$('div').hide();
         var q = $('.ing').val();
         console.log(q);
         var calFrom = $('.col-from').val();
         var calTo = $('.col-to').val();
-        var recipeUrl = `https://api.edamam.com/search?q=${q}&app_id=${app_id}&app_key=${app_key}&calories=${calFrom}-${calTo}`;
+        var recipeUrl;
+        
+        // Added this check because if the user doesn't
+        // enter, the API will fail (no calories in URL will fail)
+        if (calFrom != "" && calTo != "")
+        {
+            recipeUrl = `https://api.edamam.com/search?q=${q}&app_id=${app_id}&app_key=${app_key}&calories=${calFrom}-${calTo}`;
+        } else 
+        {
+            recipeUrl = `https://api.edamam.com/search?q=${q}&app_id=${app_id}&app_key=${app_key}`;
+        }
+
+        // for the other button that will check the nutritional value, use this URL
+        // recipeUrl = 'https://api.edamam.com/api/food-database/parser?ingr=${q}&app_id={app_id}&app_key={app_key}'
+
         fetch(recipeUrl)
             .then(function (response) {
                 return response.json();
             })
             .then(function (recipesData) {
-                console.log(recipesData);
                 var receipesDataBasedOnInput = recipesData.hits;
                 for (var i = 0; i < 5; i++){
                     var newDiv = $('<div>').attr('class', 'recipe' + i);
@@ -27,7 +45,7 @@ $(document).ready(function () {
                     var dietLabels = $('<li>').text("Diet Labels: " + receipesDataBasedOnInput[i].recipe.dietLabels);
                     var healthLabels = $('<li>').text("Health Lables: " + receipesDataBasedOnInput[i].recipe.healthLabels);
                     newDiv.append(title);
-                    var div = $('<ul>').attr('class', 'ingredients');
+                    var div = $('<ul>').attr('class', 'ingredients'+i);
                     for (var j = 0; j < receipesDataBasedOnInput[i].recipe.ingredients.length; j++){
                       
                         var ingredients = receipesDataBasedOnInput[i].recipe.ingredientLines[j];
@@ -46,7 +64,10 @@ $(document).ready(function () {
                     newDiv.append($('<br>'));
                     newDiv.append(div);
                     $('body').append(newDiv);
-                    div.css('position', 'absolute').css('right','3px');
+                    if ($(window).width() <= 860){	
+                        div.css('position', 'absolute').css('right','3px');
+                    }
+                    
                 }
 
             })

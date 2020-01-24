d170d08d7151882fa5f1c1a4490f3aa2e182b608
yan zhao <yzhao322@gmail.com>
2020-01-23 18:43:53 -0800

    fix calories range

--- project1.js
+++ project1.js
@@ -35,13 +35,16 @@ $(document).ready(function () {
                 return response.json();
             })
             .then(function (recipesData) {
+                console.log(recipesData);
                 var receipesDataBasedOnInput = recipesData.hits;
                 for (var i = 0; i < 5; i++){
                     var newDiv = $('<div>').attr('class', 'recipe' + i);
                     var title = $('<h2>').text(receipesDataBasedOnInput[i].recipe.label);
                     var img = $('<img>').attr('src', receipesDataBasedOnInput[i].recipe.image).attr('href', receipesDataBasedOnInput[i].recipe.url).attr('width', '300px').attr('height', '300px');
                     var link = $('<a>').attr('href', receipesDataBasedOnInput[i].recipe.url);
-                    var calories = $('<li>').text("Total Calories: " + receipesDataBasedOnInput[i].recipe.calories + " Kcal");
+                    var calories = receipesDataBasedOnInput[i].recipe.calories 
+                    var serving = receipesDataBasedOnInput[i].recipe.yield 
+                    var caloriesPerServing = $('<li>').text("Total Calories: " + calories / serving + " Kcal");
                     var dietLabels = $('<li>').text("Diet Labels: " + receipesDataBasedOnInput[i].recipe.dietLabels);
                     var healthLabels = $('<li>').text("Health Lables: " + receipesDataBasedOnInput[i].recipe.healthLabels);
                     newDiv.append(title);
@@ -57,7 +60,7 @@ $(document).ready(function () {
                     newDiv.append($('<br>'));
                     newDiv.append(healthLabels);
                     newDiv.append($('<br>'));
-                    newDiv.append(calories);
+                    newDiv.append(caloriesPerServing);
                     newDiv.append($('<br>'));
                     newDiv.append(link);
                     link.append(img);

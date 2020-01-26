9a15e8123a9d20af27884f5ff1e234cff9b03592
Eric Zeng <38875467+xzengeric@users.noreply.github.com>
2020-01-25 16:27:38 -0800

    added Youtube Search engine
    Added:
    searchYoutude()
    getOutput()
    
    Needs to be done:
    create appropriate css style with a class name;
    then replace # with the class name starting from line 209.

--- project1.js
+++ project1.js
@@ -28,8 +28,10 @@ $(document).ready(function () {
                 var recipeUrl = `https://api.edamam.com/search?q=${q}&app_id=${app_id}&app_key=${app_key}`;
             }
 
+
+
         }
-    
+
         getData(recipeUrl);
         function getData() {
             fetch(recipeUrl)
@@ -39,6 +41,7 @@ $(document).ready(function () {
                 .then(function (recipesData) {
                     console.log(recipesData);
                     var receipesDataBasedOnInput = recipesData.hits;
+                    
                     for (var i = 0; i < 5; i++) {
 
                         var newDiv = $('<div>').attr('class', 'recipe' + i);
@@ -58,7 +61,13 @@ $(document).ready(function () {
                             var ingredients = receipesDataBasedOnInput[i].recipe.ingredientLines[j];
                             var li = $('<li>').text(ingredients);
                             div.append(li);
+                           
                         }
+
+                        var  userInput = receipesDataBasedOnInput[i].recipe.label;
+                      
+                        searchYoutude(newDiv, userInput);
+                        
 
                         newDiv.append(title);
                         newDiv.append(dietLabels);
@@ -78,6 +87,7 @@ $(document).ready(function () {
 
                 })
 
+            
         }
         $('nav').show();
 
@@ -153,8 +163,62 @@ $(document).ready(function () {
 
             getData(recipeUrl);
 
-        
+
         })
     })
+
+//Youtube search engine. 
+
+    function searchYoutude(newDiv,userInput) {
         
+        //get from the input
+        var q = "how to cook " + userInput;
+
+        //get request on API
+        $.get(
+            "https://www.googleapis.com/youtube/v3/search", {
+            part: 'snippet, id',
+            q: q,
+            type: 'video',
+            maxResults: 1,
+            key: 'AIzaSyDT61zlLfAE3Q2q4t2VM_1i4VfRWrkwsSQ'
+        },
+            function (data) {
+
+                $.each(data.items, function (i, item) {
+                    var videoOutput = getOutput(item);
+
+                    newDiv.append(videoOutput);
+                });
+            }
+        );
+    }
+
+
+    //build output
+
+    function getOutput(item) {
+
+        var videoId = item.id.videoId;
+        var title = item.snippet.title;
+        var channelTitle = item.snippet.channelTitle;
+        var description = item.snippet.description;
+        var videoDate = item.snippet.publishedAt;
+
+
+        //Output string # is where we can repleace with the class name !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+        var displayHTML = '<li>' +
+            '<div class="#">' +
+            '<iframe class="#" width="640" height="360" src="https://www.youtube.com/embed/' + videoId + '" frameborder="0" allowfullscreen></iframe> '  +
+            '<h3>' + title + '</h3>' +
+            '<h5>By <soan class="#">' + channelTitle + '</span> on ' + videoDate + '</h5>' +
+            '<p>' + description + '</p>' +
+            '</div>' +
+            '</li>' +
+               '';
+
+        return displayHTML;
+    }
+
 })

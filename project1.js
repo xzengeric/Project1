94f9b7c30e2128ece6ac4bfd03ded8cac919df9c
Marissol Karczeski <marfkar@gmail.com>
2020-01-26 20:02:15 -0800

    ui change add slider

--- project1.js
+++ project1.js
@@ -1,4 +1,32 @@
 $(document).ready(function () {
+
+    // Slider
+    var slider = document.getElementById("slider");
+    if(slider == null)
+    {
+        alert("slider not found");
+    }
+
+    noUiSlider.create(slider, {
+     start: [50, 250],
+     connect: true,
+     tooltips: true,
+     step: 1,
+     orientation: 'horizontal', // 'horizontal' or 'vertical'
+     range: {
+       'min': 30,
+       'max': 2000
+     },
+     format: {
+        from: function(value) {
+                return parseInt(value);
+            },
+        to: function(value) {
+                return parseInt(value);
+            }
+        }
+    });
+
     $('nav').hide();
     var app_id = "98b05697";
     var app_key = "66b497d1c74de44a3ee14c66199e0618";
@@ -7,8 +35,12 @@ $(document).ready(function () {
         $('h1').hide();
         $('.input').hide();
         var q = $('.ing').val();
-        var calFrom = $('.col-from').val();
-        var calTo = $('.col-to').val();
+
+        var slider = document.getElementById("slider");
+        var calRange = slider.noUiSlider.get();
+        var calFrom = calRange[0]; //$('.col-from').val();
+        var calTo = calRange[1]; //$('.col-to').val();
+
         var foodType = $('.food-type').val();
 
         if (calFrom != "" && calTo != "") {
@@ -27,9 +59,6 @@ $(document).ready(function () {
             else {
                 var recipeUrl = `https://api.edamam.com/search?q=${q}&app_id=${app_id}&app_key=${app_key}`;
             }
-
-
-
         }
 
         getData(recipeUrl);
@@ -45,6 +74,7 @@ $(document).ready(function () {
                     for (var i = 0; i < 5; i++) {
 
                         var newDiv = $('<div>').attr('class', 'recipe' + i);
+                        //var newDiv = $('<div>').attr('class', `${recipe}${i} col s6`);
                         var title = $('<h2>').text(receipesDataBasedOnInput[i].recipe.label);
                         var img = $('<img>').attr('src', receipesDataBasedOnInput[i].recipe.image).attr('href', receipesDataBasedOnInput[i].recipe.url).attr('width', '300px').attr('height', '300px');
                         var link = $('<a>').attr('href', receipesDataBasedOnInput[i].recipe.url);
@@ -81,6 +111,7 @@ $(document).ready(function () {
                         newDiv.append(div);
                         var row = $('<div>').attr('class','row');
                         $('body').append(row);
+                        //$('.row').append(newDiv);
                         var columnLeft = $('<div>').attr('class', 'col s6').css('float', 'left');
                         columnLeft.append(newDiv);
                         row.append(columnLeft);
